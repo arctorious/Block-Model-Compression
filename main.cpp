7bb7411b50330bf4544cb8537241bb622c1256ff
tinnamchoi <tinnam.choi@gmail.com>
2023-08-13 14:56:54 +0930

    feat(input): create functions to read the first line into a std::vector<int> and print it out

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+/**
+ * @brief Read the x_count, y_count, z_count, parent_x, parent_y, parent_z from
+ * the file
+ *
+ * @param fin the input file stream
+ * @param dimensions pointer to the array to store the dimensions
+ */
+void read_dimensions(std::ifstream& fin, std::vector<int>* dimensions) {
+  std::string line;
+  std::getline(fin, line);
+  std::stringstream ss(line);
+  std::string token;
+  while (std::getline(ss, token, ',')) {
+    dimensions->push_back(std::stoi(token));
+  }
+}
+
+/**
+ * @brief Print the array
+ *
+ * @param dimensions the array to print
+ */
+void print_array(std::vector<int>& dimensions) {
+  for (int i = 0; i < dimensions.size(); i++) {
+    std::cout << dimensions[i] << " ";
+  }
+  std::cout << std::endl;
+}
+
+int main() {
+  std::ifstream fin("input.txt");
+
+  std::vector<int> dimensions;
+  read_dimensions(fin, &dimensions);
+
+  print_array(dimensions);
+
+  return 0;
+}
