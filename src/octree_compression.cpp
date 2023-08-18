3aa2dd1b542fa1e76433601c9e3afa0d15e2406c
tinnamchoi <tinnam.choi@gmail.com>
2023-08-18 18:36:50 +0930

    refactor(octree): make iterative

--- src/octree_compression.cpp
+++ src/octree_compression.cpp
@@ -14,25 +14,27 @@ void OctreeCompression::CompressBlock(int z_start, int y_start, int x_start) {
     int y_end = y_start + myDimensions->y_parent - 1;
     int x_end = x_start + myDimensions->x_parent - 1;
     
-    solve(z_start, y_start, x_start, z_end, y_end, x_end);
-}
-
-void OctreeCompression::solve(int z_start, int y_start, int x_start, int z_end, int y_end, int x_end) {
-    if (isHomogeneous(z_start, y_start, x_start, z_end, y_end, x_end)) {
-        PrintOutput(x_start, y_start, z_start+current_level, x_end-x_start+1, y_end-y_start+1, z_end-z_start+1, (*myTagTable)[(*mySlices)[z_start][y_start][x_start]]);
-    } else {
-        int z_mid = (z_start + z_end) / 2;
-        int y_mid = (y_start + y_end) / 2;
-        int x_mid = (x_start + x_end) / 2;
-        
-        solve(z_start, y_start, x_start, z_mid, y_mid, x_mid);
-        solve(z_start, y_start, x_mid+1, z_mid, y_mid, x_end);
-        solve(z_start, y_mid+1, x_start, z_mid, y_end, x_mid);
-        solve(z_start, y_mid+1, x_mid+1, z_mid, y_end, x_end);
-        solve(z_mid+1, y_start, x_start, z_end, y_mid, x_mid);
-        solve(z_mid+1, y_start, x_mid+1, z_end, y_mid, x_end);
-        solve(z_mid+1, y_mid+1, x_start, z_end, y_end, x_mid);
-        solve(z_mid+1, y_mid+1, x_mid+1, z_end, y_end, x_end);
+    std::queue<std::tuple<int, int, int, int, int, int>> q;
+    q.push(std::make_tuple(z_start, y_start, x_start, z_end, y_end, x_end));
+    while (!q.empty()) {
+        std::tie(z_start, y_start, x_start, z_end, y_end, x_end) = q.front();
+        q.pop();
+        if (isHomogeneous(z_start, y_start, x_start, z_end, y_end, x_end)) {
+            PrintOutput(x_start, y_start, z_start+current_level, x_end-x_start+1, y_end-y_start+1, z_end-z_start+1, (*myTagTable)[(*mySlices)[z_start][y_start][x_start]]);
+        } else {
+            int z_mid = (z_start + z_end) / 2;
+            int y_mid = (y_start + y_end) / 2;
+            int x_mid = (x_start + x_end) / 2;
+            
+            q.push(std::make_tuple(z_start, y_start, x_start, z_mid, y_mid, x_mid));
+            q.push(std::make_tuple(z_start, y_start, x_mid+1, z_mid, y_mid, x_end));
+            q.push(std::make_tuple(z_start, y_mid+1, x_start, z_mid, y_end, x_mid));
+            q.push(std::make_tuple(z_start, y_mid+1, x_mid+1, z_mid, y_end, x_end));
+            q.push(std::make_tuple(z_mid+1, y_start, x_start, z_end, y_mid, x_mid));
+            q.push(std::make_tuple(z_mid+1, y_start, x_mid+1, z_end, y_mid, x_end));
+            q.push(std::make_tuple(z_mid+1, y_mid+1, x_start, z_end, y_end, x_mid));
+            q.push(std::make_tuple(z_mid+1, y_mid+1, x_mid+1, z_end, y_end, x_end));
+        }
     }
 }
 
