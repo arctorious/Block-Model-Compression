3d9cac733dbf79cc133759a456758a8b194fdd50
TheRealGandhi <irhas.gill@student.adelaide.edu.au>
2023-10-20 15:26:37 +1030

    (fix) working for intro one
    Fixed a bunch of ref and pointer issues where it was overwriting things that it wasn't meant to. Pretty much working now and outperforms octree solution in time and compression. (fewer lines and like 100ms better runtime)

--- src/runlength_encoding_3D.cpp
+++ src/runlength_encoding_3D.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include <unordered_set>
 #include <iostream>
+#include <stack>
 using namespace std;
 
 
@@ -28,48 +29,82 @@ void Runlength3D::PrintBlock(OutputNode& Node)
 }
 
 // Define a custom hash function for OutputNode
+// instead of hash, keep track of ranges covered
 struct OutputNodeHash 
 {
     std::size_t operator()(const OutputNode& node) const 
     {
+
         std::size_t h1 = std::hash<int>{}(node.myXStart);
         std::size_t h2 = std::hash<int>{}(node.myYStart);
         std::size_t h3 = std::hash<int>{}(node.myZStart);
         std::size_t h4 = std::hash<char>{}(node.type);
-        return h1 ^ h2 ^ h3 ^ h4;
+        return h1 ^ h2 ^ h3 ^ h4 ;
     }
 };
 
+//returns true if A is contained in B.
+bool isContainedIn(OutputNode& A, OutputNode& B)
+{
+    return (A.myXStart >= B.myXStart 
+    && A.myYStart >= B.myYStart 
+    && A.myZStart >= B.myZStart)
+    && (A.myXStart + A.xLen <= B.myXStart + B.xLen
+    && A.myYStart + A.yLen <= B.myYStart + B.yLen
+    && A.myZStart + A.zLen <= B.myZStart + B.zLen);
+}
+
+
+// Prints all blocks that aren't contained within other blocks in linear time.
 void Runlength3D::PrintBlocks(std::vector<std::vector<std::vector<OutputNode*>>>& Blocks)
 {
-    std::unordered_set<OutputNode, OutputNodeHash> outputted;
-    PrintBlock(*Blocks[0][0][0]);
+    std::stack<OutputNode> monotonicStack;
+    std::unordered_set<OutputNode,OutputNodeHash> outputted;
     for(int z = 0; z < myDimensions->z_parent; z++)
     {
         for(int y = 0; y < myDimensions->y_parent; y++)
         {
             for(int x = 0; x < myDimensions->x_parent; x++)
             {
-                if(outputted.find(*Blocks[z][y][x]) == outputted.end() && Blocks[z][y][x]->myXStart >= 0)
+                auto& block = *Blocks[z][y][x];
+                if(outputted.find(block) == outputted.end())
                 {
-                    outputted.insert((*Blocks[z][y][x]));
-                    PrintBlock(*Blocks[z][y][x]);
-                    // x +=  Blocks[x][y][z]->xLen-1;
+                    // PrintBlock(block);
+                    outputted.insert(block);
+                    while(!monotonicStack.empty())
+                    {
+                        if(isContainedIn(monotonicStack.top(),block))
+                            monotonicStack.pop();
+                        else if(isContainedIn(block,monotonicStack.top()))
+                            break;
+                        else
+                        {
+                            monotonicStack.push(block);
+                            break;
+                        }
+                    }
+                    if(monotonicStack.empty())
+                        monotonicStack.push(block);
+                    x +=  Blocks[x][y][z]->xLen-1;
                 }
             }
         }
     }
+    while(!monotonicStack.empty())
+    {
+        PrintBlock(monotonicStack.top());
+        monotonicStack.pop();
+    }
 }
 
+// Map each index in the parent block to a subblock pointer. Update subblocks accordingly as we grow them while iterating through. This is a 2 (technically 3 for memory allocation) pass RLE solution with O(n) time complexity
 void Runlength3D::CompressBlock(int x_start, int y_start, int z_start)
 {
-    // cout << "Compressing: " << x_start << " " << y_start << " " << z_start << endl;
     // :TODO: Investigate if using a hashmap would get worse performance, if not then use it since it would have less memory usage.
     std::vector<std::vector<std::vector<OutputNode*>>> subBlocks(myDimensions->z_parent, std::vector<std::vector<OutputNode*>>(myDimensions->y_parent, std::vector<OutputNode*>(myDimensions->x_parent,nullptr)));
     
-    // Cannot use length until dimensions is casted as const so it can be calculated at compile time.
-    // const auto length = myDimensions->x_parent * myDimensions->y_parent * myDimensions->z_parent;
-    OutputNode sharedNodes[100];
+    const auto length1D = myDimensions->x_parent * myDimensions->y_parent * myDimensions->z_parent;
+    std::vector<OutputNode> sharedNodes(length1D);
 
     // initialise the subBlocks.
     int index = 0;
@@ -90,22 +125,23 @@ void Runlength3D::CompressBlock(int x_start, int y_start, int z_start)
         for(int y = 0; y < myDimensions->y_parent; y++)
         {
             //get subsequent xrow and match with previous xrows if adjacent.
-            OutputNode* start = subBlocks[z][y][0];
+            OutputNode* &start = subBlocks[z][y][0];
             start->SetStart(x_start,y+y_start,z+z_start);
             start->type = (*mySlices)[z+z_start][y+y_start][x_start];
             // Get x Blocks.
             for(int x = 1; x <  myDimensions->x_parent; x++)
             {
                 char key = (*mySlices)[z+z_start][y+y_start][x+x_start];
-                
                 // If next tagtype on row is same as the tagtype of the current sub block, map to that subBlock.
                 if(key == start->type) 
                 {
+                    // cout << "expanding " << x << y << z << endl;
+                    // PrintBlock(*start);
                     start->xLen++;
                     subBlocks[z][y][x] = start;
+                    // Handle issue when we encounter new block at the end of a row. 
                     if(x != (myDimensions->x_parent-1)) continue;
                 }
-
                 // Check if it can be matched with previous row.
                 if(y>0)
                 {
@@ -113,30 +149,48 @@ void Runlength3D::CompressBlock(int x_start, int y_start, int z_start)
                     AAAA
                     AAAB
                     AAAA
+
+                    issue was that merging doesn't occur as the previous pointers are still pointing to where start was pointing to. 
                     */
-                    auto& prevRow = subBlocks[z][y-1][x-1];
+                    // cout << "checking" << endl;
+                    auto& prevRow = (x == myDimensions->x_parent-1) ? subBlocks[z][y-1][x] : subBlocks[z][y-1][x-1];
+                    // PrintBlock(*prevRow);
                     if(prevRow->type == start->type && prevRow->xLen == start->xLen && prevRow->myXStart == start->myXStart)
                     {
+                        // cout << "merging in y at : " << x << y << z << endl;
+                        // PrintBlock(*start);
+                        // PrintBlock(*prevRow);
                         prevRow->yLen++;
+                        subBlocks[z][y][start->myXStart-x_start + start->xLen-1] = prevRow;
                         start = prevRow;
                     }
                     // If could not be matched with previous row, check if subBlock from previous row can be matched to previous slice.
-                    // If sub-block can be merged with sub-block from previous slice then merge in the z direction. This code may cause errors
-                    // because it may merge subBlocks twice. 
-                    else if( z > 0 
-                        && (prevRow->type       == subBlocks[z-1][y-1][x-1]->type)
-                        && (prevRow->myXStart   == subBlocks[z-1][y-1][x-1]->myXStart)
-                        && (prevRow->myYStart   == subBlocks[z-1][y-1][x-1]->myYStart) 
-                        && (prevRow->xLen       == subBlocks[z-1][y-1][x-1]->xLen)
-                        && (prevRow->yLen       == subBlocks[z-1][y-1][x-1]->yLen))
+                    // If sub-block can be merged with sub-block from previous slice then merge in the z direction. 
+                    if(z > 0)
                     {
-                        subBlocks[z-1][y-1][x-1]->zLen++;
-                        subBlocks[z][y-1][x-1] = subBlocks[z-1][y-1][x-1];
+                        auto& prevSlice = (x == myDimensions->x_parent-1) ? subBlocks[z-1][y-1][x] : subBlocks[z-1][y-1][x-1];
+                        // cout << "checking to merge at: " << x << y << z << endl;
+                        // PrintBlock(*prevRow);
+                        // PrintBlock(*prevSlice);
+                        if((prevRow->type       == prevSlice->type)
+                        && (prevRow->myXStart   == prevSlice->myXStart)
+                        && (prevRow->myYStart   == prevSlice->myYStart) 
+                        && (prevRow->xLen       == prevSlice->xLen)
+                        && (prevRow->yLen       == prevSlice->yLen))
+                        {
+                            // cout << "Merging z at : " << x << y << z << endl;
+                            prevSlice->zLen++;
+                            prevRow = prevSlice;
+                            // PrintBlock(*prevRow);
+                            // PrintBlock(*prevSlice);
+                        }
                     }
                 }
-                // Initialise the next block.
-                if(x == (myDimensions->x_parent-1)) continue;
+                // Initialise the next block. -- this was stuffing up at the case where the last block in a row is different to the rest of the row, so 
+                // I had to check that start's type matches the end block before skipping to account for this specific scenario.
+                if(x == (myDimensions->x_parent-1) && start->type == key) continue;
                 start = subBlocks[z][y][x];
+                start->SetStart(x+x_start,y+y_start,z+z_start);
                 start->type = key;
             }
         }
